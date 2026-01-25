ba706f397f064258d1002561bb0181ee9448985c
Jakubkol-hub <jakubkolinski3@gmail.com>
2026-01-25 14:15:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'mobywatel-cache-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    'index.html',
+    'home.html',
+    'card.html',
+    'documents.html',
+    'id.html',
+    'more.html',
+    'qr.html',
+    'services.html',
+    'assets/index.css',
+    'assets/index.js',
+    'assets/card.js',
+    'assets/bar.js',
+    'assets/id.js',
+    'images/logo.png',
+    'manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
